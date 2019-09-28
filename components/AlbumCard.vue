544cc6d307054a512868412566527ce84132c30f
AcheZeta <ache.mariana@gmail.com>
2019-09-27 19:57:40 -0500

    ADD: Bulma

---
+++ components/AlbumCard.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+    <div>
+        <h3>
+            {{ album.title }}
+        </h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "AlbumCard",
+  props: {
+    album: Object
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="albums">
     <h1>Albums de la página</h1>
-    <div v-for="album in albums" :key="album.title">
-      <h3>{{ album.title }}</h3>
-    </div>
+    <AlbumCard :album="album" v-for="album in albums" :key="album.title"/>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import env from "../config/env";
+import AlbumCard from "../components/AlbumCard"
 
 export default {
   name: "IndexPage",
+  components: {
+    AlbumCard
+  },
   data() {
     return {
       albums: []
