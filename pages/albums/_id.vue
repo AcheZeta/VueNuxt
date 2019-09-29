0ce4018e703dc183d552328f0776fe7ce3a32b45
AcheZeta <ache.mariana@gmail.com>
2019-09-28 19:44:37 -0500

    INSTALL: Vue Router and create album in Pages Folder

--- pages/albums/_id.vue
+++ pages/albums/_id.vue
@@ -1,9 +1,21 @@
 <template lang="html">
-<h1>Hola Mundo</h1>
+<h1>Album Id {{albumId}}</h1>
 </template>
 
 <script>
-export default {};
+import router from "vue-router";
+import axios from "axios";
+export default {
+  name: "AlbumIndPages",
+  data() {
+    return {
+      idAlbum: []
+    };
+  },
+  created() {
+    this.albumId = this.$route.params.id;
+  }
+};
 </script>
 
 <style scoped>
